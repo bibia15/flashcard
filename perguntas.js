8bd117135459e5714ef57fa86472ecab5bbfe1e4
bibia15 <163008110+bibia15@users.noreply.github.com>
2024-10-07 11:33:46 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    "Programação 2.0",
+    "O que é phyton?",
+    "Phyton é uma limguagem de programação"
+)
+
+
+criaCartao(
+    "Formula 1",
+    "Qual é o melhor piloto de f1 hoje em dia?'",
+    "Hamilton"
+)
+
+criaCartao(
+    "Formula 1 ",
+    "Qual é a melhor equipe da temporada?",
+    "Mclaren"
+)
+
+criaCartao(
+    "Formula 1",
+    "Aonde é o proximo grid?",
+    "Nos estados unidos"
+)
+
+criaCartao(
+    "Formula 1",
+    "Quantos dias faltam para para dia 3 de novembro(7/10)?",
+    "27 dias"
+)
+
+criaCartao(
+    "Formula 1",
+    "Que evento vai ter dia 3 de novembro?",
+    "Gp de Interlagos"
+)
+
+criaCartao(
+    "Formula 1",
+    "Apelido do seu piloto favorito?",
+    "Pimentão"
+)
